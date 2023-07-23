1de8266771e770ccb50455a05a85f1719aa35b9f
JJRoyale <98667766+JJRoyale@users.noreply.github.com>
2023-07-23 22:07:32 +0200

    Update tgackd.bat

--- tgackd.bat
+++ tgackd.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist %cd%\TGAckd mkdir %cd%\TGAckd
 if not exist %cd%\TGAckd\temp mkdir %cd%\TGAckd\temp
 for %%f in (Input/*.dds) do xtx_extract -o TGAckd/temp/%%~nf.xtx Input/%%f
 for %%f in (TGAckd/temp/*.xtx) do quickbms -o "scriptDDStoCKD.bms" TGAckd\temp\%%f TGAckd\
