427309b591b93278b6270c24ade20a26a03f8612
JJRoyale <98667766+JJRoyale@users.noreply.github.com>
2023-07-28 20:05:30 +0200

    Update pngckd.bat

--- pngckd.bat
+++ pngckd.bat
@@ -1,6 +1,5 @@
 @echo off
-if not exist %cd%\PNGckd mkdir %cd%\PNGckd
-if not exist %cd%\PNGckd\temp mkdir %cd%\PNGckd\temp
+mkdir %cd%\PNGckd\temp
 for %%f in (Input/*.dds) do xtx_extract -o PNGckd/temp/%%~nf.xtx Input/%%f
 for %%f in (PNGckd/temp/*.xtx) do quickbms -o "scriptDDStoCKD2.bms" PNGckd\temp\%%f PNGckd\
 del %cd%\PNGckd\temp\*.* /Q
